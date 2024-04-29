d961e8df4a9d24f5accfa2f2966c578006abb690
dombr32 <dombrrom@gmail.com>
2024-04-29 18:06:05 +0300

    fixed

--- TREESUM/script.js
+++ TREESUM/script.js
@@ -1,15 +1,18 @@
-function treesum (arr) {
-  let sum=0;
-  for (let i=0; i < arr.length; i++) {
 
-  let item = arr[i]
-  if ( Array.isArray(item)){
-    sum += treesum (item)
-  } else {
-    sum += item
+  function sumArray (array){
+    let sum = 0
+    for (let i=0; i < array.length; i++) {
+      const item = array[i]
+      if (Array.isArray(item)){
+      sum += sumArray(item)} else{
+      sum += item}
+    }
+    return sum 
   }
-} 
-  return sum  
-}
 
-console.log (treesum([0,5,[4],7]))
+console.log(sumArray ([4, 5, [13, [4, 5]]]))
+
+// console.log(sumArray([ 5, 7, 
+//   [ 4, [2], 8, [1,3], 2 ], 
+//   [ 9, [] ], 
+//   1, 8 ]))
