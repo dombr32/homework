c729e4dddd46cb59d4a6cad555a1c42574bb7cb4
dombr32 <dombrrom@gmail.com>
2024-05-01 13:36:18 +0300

    vowels

---
+++ VOWELS/script.js
@@ -0,0 +1,22 @@
+
+
+function vowels(){
+
+const vowel = {У:true, у:true, е:true, Е:true, Ы:true, ы:true, О:true, о:true, Э:true, э:true, я:true, Я:true, И:true, 
+    и:true, Ю:true, ю:true, Ё:true, ё:true, А:true, а:true};
+let total = 0
+
+for (let i=0; i<textarr.length; i++){
+    const word=textarr[i];
+    if (word in vowel)
+        total += 1;
+    continue;
+    
+}
+    return total
+}
+
+let text = prompt('Введите текст');
+let textarr = text.split("");
+
+ console.log (vowels(text))
