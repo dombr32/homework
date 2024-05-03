0e7cd1b9a71624fcab8ef781e04b3b4e161c5b72
dombr32 <dombrrom@gmail.com>
2024-05-03 20:53:14 +0300

    mood

--- MOOD/js.js
+++ MOOD/js.js
@@ -6,17 +6,16 @@ function mood(colorsCount) {
 const colors=[ '', 'красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый' ];
 
 console.log( 'цветов: ' + colorsCount );
-    
 const usedColors = {}
-    for ( let i=1; i<=colorsCount; i++ ) {
-        const n=randomDiap(1,7);
-        const colorName=colors[n];
-            if (!(colorName in usedColors)){
-                usedColors[colorName]=1
-                console.log( colorName )} else {
-                colorsCount++
-                }        
-    }
+for ( let i=1; i<=colorsCount; i++ ) {
+    do {
+    const n=randomDiap(1,7);
+    var colorName=colors[n]
+    } while (colorName in usedColors)
+    
+usedColors[colorName]=1;    
+console.log( colorName );
+}
 }
 
 mood(3);
